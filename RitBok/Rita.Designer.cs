22ff9c474ccbb01441c4e593a045c31cace47d1c
Jacke1012 <46431999+Jacke1012@users.noreply.github.com>
2020-09-22 19:12:49 +0200

    Add project files.

---
+++ RitBok/Rita.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace RitBok
+{
+    partial class RitClass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLinje = new System.Windows.Forms.Button();
+            this.BtnRektangel = new System.Windows.Forms.Button();
+            this.BtnEllips = new System.Windows.Forms.Button();
+            this.BtnPenna = new System.Windows.Forms.Button();
+            this.BtnÅngra = new System.Windows.Forms.Button();
+            this.tbxStorlek = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SelectColor = new System.Windows.Forms.ColorDialog();
+            this.pbxColorView = new System.Windows.Forms.PictureBox();
+            this.BtnBytFärg = new System.Windows.Forms.Button();
+            this.pbxMain = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxBrushSize = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxColorView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnLinje
+            // 
+            this.BtnLinje.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLinje.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.BtnLinje.Location = new System.Drawing.Point(29, 23);
+            this.BtnLinje.Name = "BtnLinje";
+            this.BtnLinje.Size = new System.Drawing.Size(144, 53);
+            this.BtnLinje.TabIndex = 0;
+            this.BtnLinje.Text = "Linje";
+            this.BtnLinje.UseVisualStyleBackColor = true;
+            this.BtnLinje.Click += new System.EventHandler(this.BtnLinje_Click);
+            // 
+            // BtnRektangel
+            // 
+            this.BtnRektangel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRektangel.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.BtnRektangel.Location = new System.Drawing.Point(29, 93);
+            this.BtnRektangel.Name = "BtnRektangel";
+            this.BtnRektangel.Size = new System.Drawing.Size(144, 53);
+            this.BtnRektangel.TabIndex = 1;
+            this.BtnRektangel.Text = "Rektangel";
+            this.BtnRektangel.UseVisualStyleBackColor = true;
+            this.BtnRektangel.Click += new System.EventHandler(this.BtnRektangel_Click);
+            // 
+            // BtnEllips
+            // 
+            this.BtnEllips.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEllips.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.BtnEllips.Location = new System.Drawing.Point(29, 164);
+            this.BtnEllips.Name = "BtnEllips";
+            this.BtnEllips.Size = new System.Drawing.Size(144, 53);
+            this.BtnEllips.TabIndex = 2;
+            this.BtnEllips.Text = "Ellips";
+            this.BtnEllips.UseVisualStyleBackColor = true;
+            this.BtnEllips.Click += new System.EventHandler(this.BtnEllips_Click);
+            // 
+            // BtnPenna
+            // 
+            this.BtnPenna.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPenna.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.BtnPenna.Location = new System.Drawing.Point(29, 235);
+            this.BtnPenna.Name = "BtnPenna";
+            this.BtnPenna.Size = new System.Drawing.Size(144, 53);
+            this.BtnPenna.TabIndex = 3;
+            this.BtnPenna.Text = "Penna";
+            this.BtnPenna.UseVisualStyleBackColor = true;
+            this.BtnPenna.Click += new System.EventHandler(this.BtnPenna_Click);
+            // 
+            // BtnÅngra
+            // 
+            this.BtnÅngra.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnÅngra.ForeColor = System.Drawing.Color.Red;
+            this.BtnÅngra.Location = new System.Drawing.Point(29, 356);
+            this.BtnÅngra.Name = "BtnÅngra";
+            this.BtnÅngra.Size = new System.Drawing.Size(144, 53);
+            this.BtnÅngra.TabIndex = 4;
+            this.BtnÅngra.Text = "Ångra";
+            this.BtnÅngra.UseVisualStyleBackColor = true;
+            // 
+            // tbxStorlek
+            // 
+            this.tbxStorlek.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxStorlek.Location = new System.Drawing.Point(87, 430);
+            this.tbxStorlek.Name = "tbxStorlek";
+            this.tbxStorlek.Size = new System.Drawing.Size(100, 23);
+            this.tbxStorlek.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 433);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Storlek";
+            // 
+            // SelectColor
+            // 
+            this.SelectColor.AnyColor = true;
+            // 
+            // pbxColorView
+            // 
+            this.pbxColorView.Location = new System.Drawing.Point(44, 490);
+            this.pbxColorView.Name = "pbxColorView";
+            this.pbxColorView.Size = new System.Drawing.Size(37, 33);
+            this.pbxColorView.TabIndex = 7;
+            this.pbxColorView.TabStop = false;
+            // 
+            // BtnBytFärg
+            // 
+            this.BtnBytFärg.Location = new System.Drawing.Point(87, 496);
+            this.BtnBytFärg.Name = "BtnBytFärg";
+            this.BtnBytFärg.Size = new System.Drawing.Size(54, 27);
+            this.BtnBytFärg.TabIndex = 8;
+            this.BtnBytFärg.Text = "Byt Färg";
+            this.BtnBytFärg.UseVisualStyleBackColor = true;
+            this.BtnBytFärg.Click += new System.EventHandler(this.BtnBytFärg_Click);
+            // 
+            // pbxMain
+            // 
+            this.pbxMain.Location = new System.Drawing.Point(228, 12);
+            this.pbxMain.Name = "pbxMain";
+            this.pbxMain.Size = new System.Drawing.Size(692, 516);
+            this.pbxMain.TabIndex = 9;
+            this.pbxMain.TabStop = false;
+            this.pbxMain.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ClickDetect);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 461);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Brush Size";
+            // 
+            // tbxBrushSize
+            // 
+            this.tbxBrushSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxBrushSize.Location = new System.Drawing.Point(87, 461);
+            this.tbxBrushSize.Name = "tbxBrushSize";
+            this.tbxBrushSize.Size = new System.Drawing.Size(100, 23);
+            this.tbxBrushSize.TabIndex = 10;
+            // 
+            // RitClass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 563);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxBrushSize);
+            this.Controls.Add(this.pbxMain);
+            this.Controls.Add(this.BtnBytFärg);
+            this.Controls.Add(this.pbxColorView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxStorlek);
+            this.Controls.Add(this.BtnÅngra);
+            this.Controls.Add(this.BtnPenna);
+            this.Controls.Add(this.BtnEllips);
+            this.Controls.Add(this.BtnRektangel);
+            this.Controls.Add(this.BtnLinje);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "RitClass";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.RitClass_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxColorView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLinje;
+        private System.Windows.Forms.Button BtnRektangel;
+        private System.Windows.Forms.Button BtnEllips;
+        private System.Windows.Forms.Button BtnPenna;
+        private System.Windows.Forms.Button BtnÅngra;
+        private System.Windows.Forms.TextBox tbxStorlek;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog SelectColor;
+        private System.Windows.Forms.PictureBox pbxColorView;
+        private System.Windows.Forms.Button BtnBytFärg;
+        private System.Windows.Forms.PictureBox pbxMain;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxBrushSize;
+    }
+}
+
---
+++ RitBok/Rita.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization.Configuration;
+
+namespace RitBok
+{
+    public partial class RitClass : Form
+    {
+
+        public enum Tools
+        {
+            Linje,
+            Rektangel,
+            Ellips,
+            Penna
+        }
+
+        public enum ClickState
+        {
+            Non,
+            First,
+            Second
+        }
+
+        public enum MoveOrClick
+        {
+            Move,
+            Click
+        }
+
+        delegate void DrawTool(object sender, MouseEventArgs e);
+
+        public RitClass()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap image;
+
+        DrawTool toolMethod;
+
+        ClickState currentClickState = ClickState.Non;
+        MoveOrClick moveOrClick = MoveOrClick.Click;
+        private void RitClass_Load(object sender, EventArgs e)
+        {
+            image = new Bitmap(692, 516);
+            tbxStorlek.Text = "15";
+            tbxBrushSize.Text = "15";
+        }
+
+
+        Point[] points = new Point[2];
+        Pen pen = new Pen(Color.Black);
+        float size;
+        public void ClickDetect(object sender, MouseEventArgs e)
+        {
+            size = float.Parse(tbxStorlek.Text);
+            pen.Width = float.Parse(tbxBrushSize.Text);
+            switch (currentClickState)
+            {
+                case ClickState.Non:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        currentClickState = ClickState.First;
+                    }
+                    break;
+                case ClickState.First:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        currentClickState = ClickState.Second;
+                    }
+                    else if (e.Button == MouseButtons.Right)
+                    {
+                        currentClickState = ClickState.Non;
+                    }
+                    break;
+                case ClickState.Second:
+                    break;
+                default:
+                    break;
+            }
+
+            toolMethod.Invoke(this, e);
+
+        }
+
+        private void MoveDetect(object sender, MouseEventArgs e)
+        {
+            size = float.Parse(tbxStorlek.Text);
+            pen.Width = float.Parse(tbxBrushSize.Text);
+            switch (currentClickState)
+            {
+                case ClickState.Non:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        currentClickState = ClickState.First;
+                    }
+                    break;
+                case ClickState.First:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        currentClickState = ClickState.First;
+                    }
+                    else
+                    {
+                        currentClickState = ClickState.Non;
+                    }
+                    break;
+                case ClickState.Second:
+                    break;
+                default:
+                    break;
+            }
+            DrawEllipse(sender, e);
+        }
+
+        public void DrawLine(object sender, MouseEventArgs e)
+        {
+            switch (currentClickState)
+            {
+                case ClickState.Non:
+                    break;
+                case ClickState.First:
+                    points[0] = new Point(e.X, e.Y);
+                    break;
+                case ClickState.Second:
+                    points[1] = new Point(e.X, e.Y);
+                    Graphics g = Graphics.FromImage(image);
+                    g.DrawLine(pen, points[0], points[1]);
+                    UpdatePbx();
+                    currentClickState = ClickState.Non;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void DrawEllipse(object sender, MouseEventArgs e)
+        {
+            switch (currentClickState)
+            {
+                case ClickState.First:
+                    Graphics g = Graphics.FromImage(image);
+                    g.DrawEllipse(pen, e.X - size/2, e.Y - size/2, size, size);
+                    UpdatePbx();
+                    currentClickState = ClickState.Non;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void DrawRectangle(object sender, MouseEventArgs e)
+        {
+            switch (currentClickState)
+            {
+                case ClickState.First:
+                    Graphics g = Graphics.FromImage(image);
+                    g.DrawRectangle(pen, e.X - size / 2, e.Y - size / 2, size, size);
+                    UpdatePbx();
+                    currentClickState = ClickState.Non;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void UpdatePbx()
+        {
+            pbxMain.Image = image;
+        }
+
+        private void BtnBytFärg_Click(object sender, EventArgs e)
+        {
+            DialogResult result = SelectColor.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                pen.Color = SelectColor.Color;
+            }
+            pbxColorView.BackColor = SelectColor.Color;
+        }
+
+        private void BtnRektangel_Click(object sender, EventArgs e)
+        {
+            ChangeToClickDetect();
+            toolMethod = DrawRectangle;
+        }
+
+        private void BtnLinje_Click(object sender, EventArgs e)
+        {
+            ChangeToClickDetect();
+            toolMethod = DrawLine;
+        }
+
+        private void BtnEllips_Click(object sender, EventArgs e)
+        {
+            ChangeToClickDetect();
+            toolMethod = DrawEllipse;
+        }
+
+        private void BtnPenna_Click(object sender, EventArgs e)
+        {
+            pbxMain.MouseClick -= ClickDetect;
+            pbxMain.MouseMove += MoveDetect;
+            moveOrClick = MoveOrClick.Move;
+        }
+        private void ChangeToClickDetect()
+        {
+            if (moveOrClick == MoveOrClick.Move)
+            {
+                pbxMain.MouseClick += ClickDetect;
+                pbxMain.MouseMove -= MoveDetect;
+                moveOrClick = MoveOrClick.Click;
+            }
+        }
+    }
+}
